09d02212f449e6ac66b2e05463bffa17b7a17023
James Hou <james@sparkworks.io>
2017-11-09 21:43:03 -0800

    Deprecate

--- src/aura/EventService/EventServiceHelper.js
+++ src/aura/EventService/EventServiceHelper.js
@@ -1,17 +1,5 @@
 ({
-  showToast : function(title, message, type, duration, mode) {
-    var type = (type ? type : "other");
-    var duration = (duration ? parseInt(duration) : 5000);
-    var mode = (mode ? mode : "dismissible");
-    var toastEvent = $A.get("e.force:showToast");
+  doNothing : function() {
 
-    toastEvent.setParams({
-      title: title,
-      message: message,
-      type: type,
-      duration: duration,
-      mode: mode
-    });
-    toastEvent.fire();
   },
 })
